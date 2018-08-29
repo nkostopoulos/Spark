662673fe4181dbb060efc2809173e9eaf23f5533
nkostopoulos <nkostopoulos@netmode.ntua.gr>
2018-08-29 12:29:54 +0300

    renamed files, erased pi_estimation

--- pi_estimation.py
+++
@@ -1,9 +0,0 @@
-import random
-from pyspark import *
-
-def inside(p):
-	x, y =  random.random(), random.random()
-	return x*x + y*y < 1
-
-count = sc.parallelize(xrange(0,1000000)).filter(inside).count()
-print(4.0 * count / 1000000)
